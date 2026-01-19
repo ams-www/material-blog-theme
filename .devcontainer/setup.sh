d2e144272630be954618ce676521c3ba61a8ce5f
Konsento <95830789+kons10@users.noreply.github.com>
2026-01-19 11:32:31 +0900

    Delete .devcontainer directory

--- .devcontainer/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install packages via npm
-npm install
-
-# Set git config
-git config --global core.editor "code --wait"
-git config --global user.signingkey "${HOME}/id_ed25519_sk.pub"
